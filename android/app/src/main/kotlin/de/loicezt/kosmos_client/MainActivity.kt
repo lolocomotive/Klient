7466956bba023fb83e1564fb3a0c09c485b9af62
lolocomotive <lolocomotive181@gmail.com>
2022-04-21 16:29:33 +0200

    Add back navbar transparency

--- android/app/src/main/kotlin/de/loicezt/kosmos_client/MainActivity.kt
+++ android/app/src/main/kotlin/de/loicezt/kosmos_client/MainActivity.kt
@@ -1,6 +1,13 @@
 package de.loicezt.kosmos_client
 
 import io.flutter.embedding.android.FlutterActivity
+import androidx.core.view.WindowCompat 
 
 class MainActivity: FlutterActivity() {
+    override fun onPostResume() {
+        super.onPostResume()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.navigationBarColor = 0x30000000
+        window.statusBarColor = 0
+    }
 }
